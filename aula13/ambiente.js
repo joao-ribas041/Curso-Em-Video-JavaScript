26ff4a9a4e502a98170cb75acd6f24aac986a739
João Ribas <ribas.joao1410@gmail.com>
2023-02-22 18:28:36 -0300

    do while

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,15 +1,21 @@
 var c = 1
-while (c < 6) {
+do {
+    console.log('Tudo bem?' + c)
+    c++
+} while (c <= 3)
+
+c = 1
+while (c <= 3) {
     console.log('Tudo bem?' + c)
     c++
 }
 
 
-
+/* 
+console.log('Tudo bem?90')
 console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?')
-console.log('Tudo bem?')
-console.log('Tudo bem?')
+console.log('Tudo bem?') */
