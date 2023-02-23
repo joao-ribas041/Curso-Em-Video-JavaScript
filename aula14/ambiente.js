d7cdae8831ebd0f1943ff756c19adf76d6eb0da3
João Ribas <ribas.joao1410@gmail.com>
2023-02-23 19:30:42 -0300

    for js

---
+++ aula14/ambiente.js
@@ -0,0 +1,19 @@
+/* var c = 1
+var d = 1
+var f = 1
+
+while (c <= 3) {
+    console.log(`C: ${c}`)
+    c++
+}
+
+do {
+    console.log(`D: ${d}`)
+    d++
+} while (d <= 3) */
+
+console.log(`Vai começar...`)
+for (var f = 1; f<=3; f++) {
+    console.log(`F: ${f}`)
+}
+console.log(`FIM!`)
